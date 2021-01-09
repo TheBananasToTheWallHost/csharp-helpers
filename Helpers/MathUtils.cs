95848bf07f02b9c6555dcf74b2977a1d15d7231c
Ryan Mileris <ryanmileris@yahoo.com>
2021-01-08 20:34:32 -0700

    finished implementing functions that round up and down to nearest power of 2

--- Helpers/MathUtils.cs
+++ Helpers/MathUtils.cs
@@ -7,12 +7,17 @@ namespace BananaTurtles.CSharp.Utils
         /// <summary>
         /// Finds the nearest power of 2 less than or equal to <paramref name="x"/>.
         /// </summary>
-        /// <param name="x">an integer</param>
+        /// <param name="x">An integer</param>
         /// <returns>The nearest power of 2 less than or equal to <paramref name="x"/> or
         /// 0 if there are no powers of 2 less than or equal to <paramref name="x"/>.</returns>
         public static int FloorToPowerOfTwo(int x) {
             if(x <= 0) {
                 return 0;
+            }
+
+            // if it's a power of 2 return it
+            if((x & (x - 1)) == 0){
+                return x;
             }
 
             for(int i = 1; i < 32; i*=2) {
@@ -22,16 +27,33 @@ namespace BananaTurtles.CSharp.Utils
         }
 
         /// <summary>
-        /// Finds the nearest power of 2 greater than or equal to <paramref name="x"/>
+        /// Finds the nearest power of 2 greater than or equal to <paramref name="x"/>. It may also return
+        /// <see cref="Int32.MaxValue"/> if <paramref name="strict"/> is false.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="strict"></param>
+        /// <param name="x">An integer</param>
+        /// <param name="strict">If true, the function will only return a power of 2, otherwise it may return the MaxValue of
+        /// <see cref="Int32.MaxValue"/></param>
         /// <returns></returns>
-        public static int CeilingToPowerOfTwo(int x, bool strict) {
+        public static int CeilingToPowerOfTwo(int x, bool strict = true) {
             if(x <= 0) {
                 return 1;
             }
 
+            if(strict && x > 0x40_00_00_00){
+                return 0x40_00_00_00;
+            }
+
+            if(!strict && x > 0x40_00_00_00){
+                return Int32.MaxValue;
+            }
+
+            // if it's a power of 2 return it
+            if((x & (x - 1)) == 0){
+                return x;
+            }
+
+            x <<= 1;
+            return FloorToPowerOfTwo(x);
 
         }
     }
